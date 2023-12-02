ae03f4ccfde5b3da88e14a218bedfce55ff8c0a5
dkshs <88355080+dkshs@users.noreply.github.com>
2023-12-02 17:27:11 -0300

    update: improving testing

--- test/index.ts
+++ test/index.ts
@@ -4,6 +4,8 @@ import fs from "fs-extra";
 
 import { logger } from "./logger";
 
+const PATTERN = /{{(\s?dkcutter)[.](.*?)}}/;
+
 const SUPPORTED_COMBINATIONS = [
   { useHusky: true },
   { useHusky: false },
@@ -22,11 +24,46 @@ const SUPPORTED_COMBINATIONS = [
 const UNSUPPORTED_COMBINATIONS = [{ database: "XXXXXX" }];
 const INVALID_SLUGS = ["", " ", "Test", "teSt", "tes1@", "t!es"];
 
-async function generateProject(args: string[] = []) {
+/**
+ * Build a list containing absolute paths to the generated files.
+ */
+function buildFilesList(baseDir: string) {
+  const files = fs.readdirSync(baseDir);
+  const paths: string[] = [];
+  files.forEach((file) => {
+    const filePath = path.join(baseDir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      paths.push(...buildFilesList(filePath));
+    } else {
+      paths.push(filePath);
+    }
+  });
+  return paths;
+}
+
+/**
+ * Method to check all paths have correct substitutions.
+ */
+function checkPaths(paths: string[]) {
+  for (const path of paths) {
+    const content = fs.readFileSync(path, "utf-8");
+    const matches = content.match(PATTERN);
+    if (matches) {
+      throw new Error(
+        `Found match in ${path} at line ${matches.index} with value ${matches[0]}`,
+      );
+    }
+  }
+}
+
+async function generateProject(args: string[] = [], output: string = ".test") {
   logger.info(
     `Generating project ${args[1]} with args: ${args.slice(2).join(" ")}`,
   );
-  await execa("pnpm", ["generate", ...args, "-y"]);
+  await execa("pnpm", ["generate", "-o", output, ...args, "-y"]);
+  const paths = buildFilesList(path.join(output, args[1]));
+  checkPaths(paths);
   logger.success(`✓ Project ${args[1]} generated`);
 }
 
@@ -40,22 +77,6 @@ function generateRandomString(n: number) {
   return result;
 }
 
-async function runLinters(project: string) {
-  try {
-    const ignore = ["--ignore-path", path.resolve(".prettierignore")];
-    const eslintCmd = ["eslint", "."];
-    const prettierCmd = ["prettier", ".", "-c", ...ignore];
-    await execa("pnpm", eslintCmd, { cwd: project });
-    await execa("pnpm", prettierCmd, { cwd: project });
-    logger.success("✓ Linting passed");
-    logger.break();
-  } catch (e) {
-    logger.error("Linting failed");
-    logger.error(e.message);
-    process.exit(1);
-  }
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function constructArgs(combination: { [key: string]: any }) {
   const args = ["--projectName", generateRandomString(8)];
@@ -65,20 +86,45 @@ function constructArgs(combination: { [key: string]: any }) {
   return args;
 }
 
+async function testCMDPassesFunc(
+  cmd: string,
+  projectDir: string,
+  args: string[] = [],
+) {
+  const cmdCapitalized = cmd.charAt(0).toUpperCase() + cmd.slice(1);
+  const result = await execa(cmd, args, { cwd: projectDir });
+  if (result.failed) {
+    throw new Error(`${cmdCapitalized} failed`);
+  }
+  logger.success(`✓ ${cmdCapitalized} passed`);
+}
+
+async function testEslintPasses(projectDir: string) {
+  await testCMDPassesFunc("eslint", projectDir);
+}
+
+async function testPrettierPasses(projectDir: string) {
+  const ignore = ["--ignore-path", path.resolve(".prettierignore")];
+  const args = [".", "-c", ...ignore];
+  await testCMDPassesFunc("prettier", projectDir, args);
+}
+
 async function main() {
   let test = ".test";
   await fs.ensureDir(test);
-  test = path.resolve(".test");
+  test = path.resolve(test);
   let testsPassed = 0;
 
   for (const combination of SUPPORTED_COMBINATIONS) {
     const args = constructArgs(combination);
     try {
       await generateProject(args);
-      const project = path.resolve(args[1]);
-      await runLinters(project);
-      await fs.move(project, path.resolve(test, args[1]));
-      testsPassed += 1;
+      const projectDir = path.join(test, args[1]);
+      await testEslintPasses(projectDir);
+      await testPrettierPasses(projectDir);
+      logger.success(`✓ All checks passed for project ${args[1]}`);
+      logger.break();
+      testsPassed += 3;
     } catch (e) {
       logger.error(
         `Failed to generate project ${args[1]} with args: ${args
