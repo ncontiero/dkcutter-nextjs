3bbd72ed27f5fe6ef112ffb6153827a4b3449f01
dkshs <88355080+dkshs@users.noreply.github.com>
2025-02-25 17:40:47 -0300

    chore(hooks): improve `dkcutter.update()` codes in preGenProject

--- hooks/preGenProject.ts
+++ hooks/preGenProject.ts
@@ -9,10 +9,10 @@ const ctx = {
 };
 
 // Removing cmd `run` when the package manager is `yarn` or `pnpm`.
-// {% if ['pnpm', 'yarn'].includes(dkcutter.pkgManager) %}
-// {{ dkcutter.update('_lintScript', dkcutter._lintScript|replace('run ', '')) }}
-// {{ dkcutter.update('_preCommit', dkcutter._preCommit|replace('run ', '')) }}
-// {{ dkcutter.update('_commitMsg', dkcutter._commitMsg|replace('run ', '')) }}
+// {% if dkcutter.pkgManager in ["pnpm", "yarn"] %}
+// {{ dkcutter.update("_lintScript", dkcutter._lintScript|replace("run ", "")) }}
+// {{ dkcutter.update("_preCommit", dkcutter._preCommit|replace("run ", "")) }}
+// {{ dkcutter.update("_commitMsg", dkcutter._commitMsg|replace("run ", "")) }}
 // {% endif %}
 
 export function validateProject({ ctx }: { ctx: unknown }) {
