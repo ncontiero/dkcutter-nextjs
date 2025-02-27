79d8aab793c133115f5494ea2fe17ee0a894500a
dkshs <88355080+dkshs@users.noreply.github.com>
2025-02-27 16:24:06 -0300

    feat(template): add `typecheck` and `check` scripts
    * chore: improve other variables and scripts

--- hooks/preGenProject.ts
+++ hooks/preGenProject.ts
@@ -8,13 +8,6 @@ const ctx = {
   useLintStaged: toBoolean("{{ dkcutter.useLintStaged }}"),
 };
 
-// Removing cmd `run` when the package manager is `yarn` or `pnpm`.
-// {% if dkcutter.pkgManager in ["pnpm", "yarn"] %}
-// {{ dkcutter.update("_lintScript", dkcutter._lintScript|replace("run ", "")) }}
-// {{ dkcutter.update("_preCommit", dkcutter._preCommit|replace("run ", "")) }}
-// {{ dkcutter.update("_commitMsg", dkcutter._commitMsg|replace("run ", "")) }}
-// {% endif %}
-
 export function validateProject({ ctx }: { ctx: unknown }) {
   try {
     z.object({
