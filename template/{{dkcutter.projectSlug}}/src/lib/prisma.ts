0701fbfc921d4095e168322f51be72ed529b31ae
Nicolas Contiero <git@ncontiero.com>
2025-09-10 22:35:50 -0300

    refactor: use new prisma engine

--- template/{{dkcutter.projectSlug}}/src/lib/prisma.ts
+++ template/{{dkcutter.projectSlug}}/src/lib/prisma.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "@/env";
+import { PrismaClient } from "./generated/prisma/client";
 
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
