061373401bbebcf077d6d1f65c06ecbf735dd2b7
Nicolas Contiero <git@ncontiero.com>
2025-09-10 22:45:14 -0300

    refactor: update some config files

--- template/{{dkcutter.projectSlug}}/eslint.config.js
+++ template/{{dkcutter.projectSlug}}/eslint.config.js
@@ -1,3 +1,5 @@
 import { ncontiero } from "@ncontiero/eslint-config";
 
-export default ncontiero();
+export default ncontiero({
+  ignores: ["./src/lib/generated/**"],
+});
---
+++ template/{{dkcutter.projectSlug}}/next.config.ts
@@ -0,0 +1,6 @@
+import type { NextConfig } from "next";
+import "./src/env";
+
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
