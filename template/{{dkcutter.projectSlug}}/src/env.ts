e333ffdff1f0621fff818a132853ba76f02af3b9
Nicolas Contiero <git@ncontiero.com>
2025-10-31 21:27:32 -0300

    fix(env): update schema validation to use z.url() for URL fields

--- template/{{dkcutter.projectSlug}}/src/env.ts
+++ template/{{dkcutter.projectSlug}}/src/env.ts
@@ -12,7 +12,7 @@ export const env = createEnv({
       .default("development"),
 {%- if dkcutter.database != 'none' %}
     // Database ({{ dkcutter.database | capitalize }})
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.url(),
 {%- endif %}
 {%- if dkcutter.authProvider == 'nextAuth' %}
     // NextAuth.Js
@@ -25,7 +25,7 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
+      process.env.VERCEL ? z.string() : z.url(),
     ),
     // Add ` on ID and SECRET if you want to make sure they're not empty
     DISCORD_CLIENT_ID: z.string(),
