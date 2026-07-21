c4b07535d3404d5530474fb7c4fad1d82017f82c
Nicolas Contiero <git@ncontiero.com>
2026-07-20 21:23:53 -0300

    chore(deps): update package managers

--- hooks/utils/getPkgManagerVersion.ts
+++ hooks/utils/getPkgManagerVersion.ts
@@ -3,9 +3,9 @@ import { logger } from "dkcutter/utils";
 import { x } from "tinyexec";
 
 const pkgManagersDefaultVersions: Record<PackageManager, string> = {
-  npm: "npm@11.18.0",
-  pnpm: "pnpm@11.9.0",
-  yarn: "yarn@4.16.0",
+  npm: "npm@12.0.1",
+  pnpm: "pnpm@11.15.1",
+  yarn: "yarn@4.17.0",
   bun: "bun@1.3.14",
 };
 
